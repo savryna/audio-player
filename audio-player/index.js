0a071577c8db8d4b963d2cdf5150205db8c9c487
Anastasia Savrukhina <assavr0@gmail.com>
2023-09-23 16:44:08 +0300

    fix: fix timetape

--- audio-player/index.js
+++ audio-player/index.js
@@ -13,6 +13,7 @@ const blockPlayer = document.querySelector('.container-audio')
 const album = document.querySelector('.container-audio-img')
 const background = document.querySelector('.background')
 
+
 // 
 // play pause audio
 function playAudio() {
@@ -20,6 +21,8 @@ function playAudio() {
   audio.play();
   path.setAttribute('d', 'M10 19C14.9706 19 19 14.9706 19 10C19 5.02944 14.9706 1 10 1C5.02944 1 1 5.02944 1 10C1 14.9706 5.02944 19 10 19ZM10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM11 14V7H12V14H11ZM8 7V14H9V7H8Z');
   play.classList.remove('pause')
+
+ 
 }
 
 function pauseAudio() {
@@ -40,15 +43,27 @@ play.addEventListener('click', controlAudio)
 
 // progress
 
+
+
 function progressTime() {
-  if(audio.play()) {
-    setInterval( () => {
+  if (audio.play()) {
+    setInterval(() => {
       range.value = audio.currentTime;
-    },500);
+      let audioDuration = audio.duration;
+      let totalMin = Math.floor(audioDuration / 60);
+      let totalSec = Math.floor(audioDuration % 60);
+      if (totalSec < 10) {
+        totalSec = `0${totalSec}`;
+      }
+      duration.innerText = `${totalMin}:${totalSec}`;
+      range.setAttribute('max', `${audioDuration}`)
+
+      
+    }, 500);
   }
 }
 
-range.onchange = function() {
+range.onchange = function () {
   audio.play();
   audio.currentTime = range.value;
   path.setAttribute('d', 'M10 19C14.9706 19 19 14.9706 19 10C19 5.02944 14.9706 1 10 1C5.02944 1 1 5.02944 1 10C1 14.9706 5.02944 19 10 19ZM10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM11 14V7H12V14H11ZM8 7V14H9V7H8Z');
@@ -66,7 +81,7 @@ let songsIndex = 0;
 function currentSong(song) {
   author.innerHTML = song;
   songName.innerHTML = songsName[songsIndex];
-  audioSrc.src = `./assets/audio/${song}.mp3`;
+  audio.src = `./assets/audio/${song}.mp3`;
   album.src = `./assets/img/${song}.jpg`;
   background.src = `./assets/img/${song}.jpg`;
   // author.href = `https://www.youtube.com/watch?v=FUdteCBRX9c`;
@@ -87,7 +102,7 @@ function nextSong() {
 
 function prevSong() {
   songsIndex--;
-  if(songsIndex < 0) {
+  if (songsIndex < 0) {
     songsIndex = songsAuthor.length - 1;
   }
   currentSong(songsAuthor[songsIndex])
@@ -100,3 +115,24 @@ prevBtn.addEventListener('click', prevSong)
 
 // loop
 audio.addEventListener('ended', nextSong)
+
+
+// 
+// let audioCurrent = audio.currentTime;
+// let currentMin = Math.floor(audioCurrent / 60);
+// let currentSec = Math.floor(audioCurrent % 60);
+// if (currentSec < 10) {
+//   currentSec = `0${currentSec}`;
+// }
+// current.innerText = `${currentMin}:${currentSec}`;
+
+audio.addEventListener('timeupdate', (e) => {
+  const audioCurrent = e.target.currentTime;
+  let currentMin = Math.floor(audioCurrent / 60);
+let currentSec = Math.floor(audioCurrent % 60);
+if (currentSec < 10) {
+  currentSec = `0${currentSec}`;
+}
+current.innerText = `${currentMin}:${currentSec}`;
+
+})
