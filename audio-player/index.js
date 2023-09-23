bcdb2dc5993a259f873b15d669a1425d74b1fc06
Anastasia Savrukhina <assavr0@gmail.com>
2023-09-23 16:07:17 +0300

    feat: add prev next function

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,37 +1,49 @@
-const audio = document.querySelector('.audio-1');
-const audioSrc = document.querySelector('.audio-source');
+const audio = document.querySelector('.audio');
+const audioSrc = document.querySelector('audio > source');
 const play = document.querySelector('.btn-play');
 const range = document.querySelector('.range');
 const path = document.querySelector('.path');
 const current = document.querySelector('.current');
 const duration = document.querySelector('.remaining');
-const nextAudio = document.querySelector('.btn-right');
+const nextBtn = document.querySelector('.btn-right');
+const prevBtn = document.querySelector('.btn-left');
 const author = document.querySelector('.author-name');
 const songName = document.querySelector('.song-name');
+const blockPlayer = document.querySelector('.container-audio')
+const album = document.querySelector('.container-audio-img')
+const background = document.querySelector('.background')
 
+// 
 // play pause audio
 function playAudio() {
+  audio.load()
+  audio.play();
+  path.setAttribute('d', 'M10 19C14.9706 19 19 14.9706 19 10C19 5.02944 14.9706 1 10 1C5.02944 1 1 5.02944 1 10C1 14.9706 5.02944 19 10 19ZM10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM11 14V7H12V14H11ZM8 7V14H9V7H8Z');
+  play.classList.remove('pause')
+}
+
+function pauseAudio() {
+  audio.pause();
+  path.setAttribute('d', 'M19 10C19 14.9706 14.9706 19 10 19C5.02943 19 1 14.9706 1 10C1 5.02943 5.02943 1 10 1C14.9706 1 19 5.02943 19 10ZM20 10C20 15.5229 15.5228 20 10 20C4.47716 20 0 15.5229 0 10C0 4.47715 4.47716 0 10 0C15.5228 0 20 4.47715 20 10ZM7.5 14.3301L15 10L7.5 5.66988V14.3301Z')
+  play.classList.add('pause')
+}
+
+function controlAudio() {
   if (play.classList.contains('pause')) {
-    audio.play();
-    path.setAttribute('d', 'M10 19C14.9706 19 19 14.9706 19 10C19 5.02944 14.9706 1 10 1C5.02944 1 1 5.02944 1 10C1 14.9706 5.02944 19 10 19ZM10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM11 14V7H12V14H11ZM8 7V14H9V7H8Z');
-    play.classList.remove('pause')
+    playAudio()
   } else {
-    audio.pause();
-    path.setAttribute('d', 'M19 10C19 14.9706 14.9706 19 10 19C5.02943 19 1 14.9706 1 10C1 5.02943 5.02943 1 10 1C14.9706 1 19 5.02943 19 10ZM20 10C20 15.5229 15.5228 20 10 20C4.47716 20 0 15.5229 0 10C0 4.47715 4.47716 0 10 0C15.5228 0 20 4.47715 20 10ZM7.5 14.3301L15 10L7.5 5.66988V14.3301Z')
-    play.classList.add('pause')
+    pauseAudio()
   }
 }
 
-play.addEventListener('click', playAudio)
+play.addEventListener('click', controlAudio)
 
 // progress
+
 function progressTime() {
   if(audio.play()) {
     setInterval( () => {
       range.value = audio.currentTime;
-      current.innerHTML = (audio.currentTime).toFixed(2) / 60;
-      duration.innerHTML = (((audio.duration).toFixed(2) - (audio.currentTime).toFixed(2)) / 60);
-      console.log(audio.currentTime)
     },500);
   }
 }
@@ -44,18 +56,47 @@ range.onchange = function() {
 
 progressTime()
 
-nextAudio.addEventListener('click', () => {
 
-})
 
 // prev next song
-const songs = ['joji', 'shortparis'];
+const songsAuthor = ['Joji', 'Shortparis'];
+const songsName = ['Like you do', 'Страшно']
 let songsIndex = 0;
 
-function changeSong(song) {
+function currentSong(song) {
   author.innerHTML = song;
-  audioSrc.src = `audio${song}.mp3`;
-  
+  songName.innerHTML = songsName[songsIndex];
+  audioSrc.src = `./assets/audio/${song}.mp3`;
+  album.src = `./assets/img/${song}.jpg`;
+  background.src = `./assets/img/${song}.jpg`;
+  // author.href = `https://www.youtube.com/watch?v=FUdteCBRX9c`;
+  // songName.href = `https://www.youtube.com/watch?v=FUdteCBRX9c`;
+  blockPlayer.setAttribute('data-shadow', `${song}`);
 }
 
-nextAudio.addEventListener('click', changeSong)
+currentSong(songsAuthor[songsIndex])
+
+function nextSong() {
+  songsIndex++;
+  if (songsIndex > songsAuthor.length - 1) {
+    songsIndex = 0;
+  }
+  currentSong(songsAuthor[songsIndex])
+  playAudio();
+}
+
+function prevSong() {
+  songsIndex--;
+  if(songsIndex < 0) {
+    songsIndex = songsAuthor.length - 1;
+  }
+  currentSong(songsAuthor[songsIndex])
+  playAudio();
+}
+
+
+nextBtn.addEventListener('click', nextSong)
+prevBtn.addEventListener('click', prevSong)
+
+// loop
+audio.addEventListener('ended', nextSong)
